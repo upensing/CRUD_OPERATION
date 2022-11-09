ec57dd0a9b03d9f79a62960713bb7e9e14f30291
upendra <upensing@gmail.com>
2022-11-09 21:57:50 +0530

    initial commit

---
+++ employee_project/employee_register/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class position(models.Model):
+    title=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class Employee(models.Model):
+    fullname=models.CharField(max_length=100)
+    emp_code=models.CharField(max_length=5)
+    mobile=models.CharField(max_length=15)
+    position=models.ForeignKey(position,on_delete=models.CASCADE)
+
